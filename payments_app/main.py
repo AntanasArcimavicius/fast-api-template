b1e932b4a06500d6042e958e208ee13c38f0a672
Anthony A <arcimavic@gmail.com>
2024-05-08 11:51:44 +0300

    add basic test

--- payments_app/main.py
+++ payments_app/main.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, status
+from sqlalchemy.orm import Session
+from sqlalchemy import text
 
-from payments_app.adapters.database import engine
+from payments_app.adapters.database import engine, get_db
 from payments_app.models import Base
 from payments_app.routers import auth, transactions
 
@@ -16,3 +18,12 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(auth.router)
 app.include_router(transactions.router)
+
+
+@app.get("/healthcheck", status_code=status.HTTP_200_OK, dependencies=[Depends(get_db)])
+async def healthcheck(db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1"))
+        return {"status": "OK", "database": "OK"}
+    except Exception:
+        return {"status": "ERROR", "database": "Failure"}
